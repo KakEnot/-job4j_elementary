73cd0d4962893e4c7dd0cd42d59aa9aeee9780fc
KakEnot <k.a.isaeva@icloud.com>
2022-08-08 21:51:24 +0400

    add methods difference and devision in MathFunction add methods differenceAndDevision and  allSum in MathCalculator

---
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -0,0 +1,27 @@
+package ru.job4j.calculator;
+
+import static ru.job4j.math.MathFunction.*;
+
+public class MathCalculator {
+
+    public static double sumAndMultiply(double first, double second) {
+        return sum(first, second)
+                + multiply(first, second);
+    }
+
+    public static double differenceAndDevision(double first, double second) {
+        return difference(first, second) + devision(first, second);
+    }
+
+    public static double allSum(double first, double second) {
+        return sum(first, second) + multiply(first, second)
+                + difference(first, second) + devision(first, second);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
+    }
+}
+/*- один будет считать сумму двух операций из класса MathFunction: разность двух чисел и деление двух чисел.
+
+- второй метод будет рассчитывать сумму вычисления всех четырех операций из класса MathFunction.*/
