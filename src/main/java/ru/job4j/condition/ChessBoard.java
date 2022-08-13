b700dd70d895cfa8a35daf18106fdc9b5ba4122f
KakEnot <k.a.isaeva@icloud.com>
2022-08-13 23:58:10 +0400

    change class ChessBoard

--- src/main/java/ru/job4j/condition/ChessBoard.java
+++ src/main/java/ru/job4j/condition/ChessBoard.java
@@ -6,8 +6,7 @@ public class ChessBoard {
         int rsl = 0;
         if (isValid(x1) && isValid(x2)
                 && isValid(y1) && isValid(y2)) {
-            rsl = (Math.abs(x2 - x1) == Math.abs(y2 - y1))
-                    ? Math.abs(x2 - x1) : 0;
+            rsl = (Math.abs(x2 - x1) == Math.abs(y2 - y1)) ? Math.abs(x2 - x1) : 0;
         }
         return rsl;
     }
