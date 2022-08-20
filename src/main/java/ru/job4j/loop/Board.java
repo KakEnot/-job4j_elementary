798494dffa210bf202c30aa0c86f44725a01289a
KakEnot <k.a.isaeva@icloud.com>
2022-08-20 20:35:17 +0400

    change class Board

--- src/main/java/ru/job4j/loop/Board.java
+++ src/main/java/ru/job4j/loop/Board.java
@@ -4,8 +4,9 @@ public class Board {
     public static void paint(int width, int height) {
         for (int i = 0; i < width; i++) {
             for (int j = 0; j < height; j++) {
-                if ((i + j) % 2 == 0) System.out.print("X");
-                else {
+                if ((i + j) % 2 == 0) {
+                    System.out.print("X");
+                } else {
                     System.out.print(" ");
                 }
             }
