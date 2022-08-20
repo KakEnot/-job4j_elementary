4ffdb01542a59c02805f5368d295c2bff6fb24b5
KakEnot <k.a.isaeva@icloud.com>
2022-08-21 00:48:06 +0400

    change class ArrayDefinition (add arr names)

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -9,5 +9,11 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен: " + ages.length);
         System.out.println("Размер массива равен: " + surnames.length);
         System.out.println("Размер массива равен: " + prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Тинки-Винки";
+        names[1] = "Дипси";
+        names[2] = "Ляля";
+        names[3] = "По";
     }
 }
