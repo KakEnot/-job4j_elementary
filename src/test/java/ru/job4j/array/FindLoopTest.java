dfeddc20f6605defc0c866ae04294573f9ca828d
KakEnot <k.a.isaeva@icloud.com>
2022-08-21 11:59:21 +0400

    change class FindLoop

--- src/test/java/ru/job4j/array/FindLoopTest.java
+++ src/test/java/ru/job4j/array/FindLoopTest.java
@@ -21,8 +21,6 @@ public class FindLoopTest {
         int result = FindLoop.indexOf(data, el);
         int expected = -1;
         assertThat(result).isEqualTo(expected);
-        /* Здесь нужно дописать тест, когда в массиве не найдено число 10.
-        Можно искать любое другое число, корректно изменив при этом название теста */
     }
 
     @Test
