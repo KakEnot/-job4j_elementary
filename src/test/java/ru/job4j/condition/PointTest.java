08fe271a2d3319335997325a3bd6b502833e6978
KakEnot <k.a.isaeva@icloud.com>
2022-10-08 19:22:33 +0400

    change class Point

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -48,4 +48,29 @@ public class PointTest {
         Assert.assertEquals(expected, out, 0.01);
     }
 
+    @Test
+    public void when40to30to20then4() {
+        double expected = 6.4;
+        int x1 = -2;
+        int y1 = -4;
+        int z1 = 5;
+        int x2 = -2;
+        int y2 = 0;
+        int z2 = 0;
+        double out = new Point(x1, y1, z1).distance3d(new Point(x2, y2, z2));
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when00to00to00then0() {
+        double expected = 0;
+        int x1 = 0;
+        int y1 = 0;
+        int x2 = 0;
+        int y2 = 0;
+        int z1 = 0;
+        int z2 = 0;
+        double out = new Point(x1, y1, z1).distance3d(new Point(x2, y2, z2));
+        Assert.assertEquals(expected, out, 0.01);
+    }
 }
