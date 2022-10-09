4d705426607caee9269118bc7b4016430985cb61
KakEnot <k.a.isaeva@icloud.com>
2022-10-09 15:19:15 +0400

    change class Max

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -7,12 +7,10 @@ public class Max {
     }
 
     public static int max(int one, int two, int three) {
-        int temp = max(one, two);
-        return max(temp, three);
+        return max(max(one, two), three);
     }
 
     public static int max(int one, int two, int three, int four) {
-        int temp = max(one, two, three);
-        return max(temp, four);
+        return max(max(one, two, three), four);
     }
 }
